4e0f5f1d07e7bf83a1961f84925d73c8abb92873
Nik <shaione3@gmail.com>
2018-02-12 19:28:36 +0000

    Done.

--- index.js
+++ index.js
@@ -7,23 +7,39 @@ function nestedTarget(){
   return document.querySelector('#nested .target');
 }
 
+// function increaseRankBy(n){
+//
+//   const rankedLists = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+//
+//   for(let i = 0; i < rankedLists.length; i++){
+//     rankedLists[i].innerHTML = parseInt(rankedLists[i].innerHTML, 10) + n;
+//   }
+//
+// }
+
+// Answer above was my solution, but below solution doesn't require targeting the specific child selector when defining the rankedLists variable
+
 function increaseRankBy(n){
 
-  const rankedLists = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  const rankedLists = document.querySelectorAll('.ranked-list');
 
   for(let i = 0; i < rankedLists.length; i++){
-    rankedLists[i].innerHTML = parseInt(rankedLists[i].innerHTML, 10) + n;
-  }
+    let rankedListsChildren = rankedLists[i].children
 
+    for(let j = 0; j < rankedListsChildren.length; j++){
+      rankedListsChildren[j].innerHTML = parseInt(rankedListsChildren[j].innerHTML, 10) + n;
+    }
+  }
 }
 
 function deepestChild(){
 
-  let current = document.getElementById('grand-node');
+  let node = document.getElementById('grand-node');
+  let nextNode = node.children[0];
 
-    for(let i = 0; i < 4; i++){
-      current = current.querySelector('div');
-    }
-
-return current;
+  while(nextNode){
+    node = nextNode;
+    nextNode = node.children[0];
+  }
+  return node;
 }
